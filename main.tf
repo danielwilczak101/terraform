a8e6a74a5fcc70d8809a3316e6cfc6ee499e38e7
danielwilczak101 <wilczakd@breakthrubev.com>
2023-04-08 01:16:48 -0500

    Updated

--- main.tf
+++ main.tf
@@ -11,7 +11,25 @@ provider "snowflake" {
   // required
   username = "danielwilczak"
   account  = "op06195" # the Snowflake account identifier
-
+  region = "ca-central-1.aws"
   // optional, exactly one must be set
-  password               = "Dmw0234567!@"
+  password  = "Dmw0234567!@"
+}
+
+resource "snowflake_database" "AZURE_PIPELINE_PRD" {
+  name                        = "AZURE_PIPELINE_PRD"
+  comment                     = "Production azure account for pipelining."
+  data_retention_time_in_days = 3
+}
+
+resource "snowflake_database" "AZURE_PIPELINE_TST" {
+  name                        = "AZURE_PIPELINE_TST"
+  comment                     = "Test azure account for pipelining."
+  data_retention_time_in_days = 3
+}
+
+resource "snowflake_database" "AZURE_PIPELINE_DE" {
+  name                        = "AZURE_PIPELINE_DEV"
+  comment                     = "Dev azure account for pipelining."
+  data_retention_time_in_days = 3
 }
