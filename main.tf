0757b67acc1a8efb76cf9cbf5a5e1d1e56703140
danielwilczak101 <wilczakd@breakthrubev.com>
2023-04-08 20:18:25 -0500

    Updated

--- main.tf
+++ main.tf
@@ -33,13 +33,13 @@ provider "snowflake" {
   region = var.SNOWFLAKE_REGION
 }
 
-resource "snowflake_database" "database" {
+resource "snowflake_database" "database_1" {
   // 
   name    = "DEMO_JACK"
   comment = "Database for me Terraform demo"
 }
 
-resource "snowflake_database" "database" {
+resource "snowflake_database" "database_2" {
   // 
   name    = "DEMO_DAN"
   comment = "Database for me Terraform demo"
