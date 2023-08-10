7d9e868bb5fa17d827126e53a3cdae4726c75ff3
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2023-08-10 04:48:02 +0000

    Testing python model

--- main.tf
+++ main.tf
@@ -54,23 +54,47 @@ resource "snowflake_procedure" "proc" {
   name     = "SAMPLE_PROC"
   database = snowflake_database.db.name
   schema   = snowflake_schema.schema.name
-  language = "JAVASCRIPT"
-  arguments {
-    name = "arg1"
-    type = "varchar"
-  }
-  arguments {
-    name = "arg2"
-    type = "DATE"
-  }
-  comment             = "Procedure with 2 arguments"
-  return_type         = "VARCHAR"
-  execute_as          = "CALLER"
-  return_behavior     = "IMMUTABLE"
-  null_input_behavior = "RETURNS NULL ON NULL INPUT"
+  language = "PYTHON"
+  runtime_version = "3.8"
+  comment             = "Creates a stored procedure that trains a linear regression model"
+  return_type         = "VARIANT"
+  packages = ["snowflake-snowpark-python", "scikit-learn", "joblib"]
   statement           = <<EOT
-  var X=1
-  return X
+  import os
+  from sklearn.compose import ColumnTransformer
+  from sklearn.pipeline import Pipeline
+  from sklearn.preprocessing import PolynomialFeatures
+  from sklearn.preprocessing import StandardScaler
+  from sklearn.linear_model import LinearRegression
+  from sklearn.model_selection import train_test_split, GridSearchCV
+  from joblib import dump
+
+  def main(session):
+    # Load features
+    df = session.table('MARKETING_BUDGETS_FEATURES').to_pandas()
+    X = df.drop('REVENUE', axis = 1)
+    y = df['REVENUE']
+
+    # Split dataset into training and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 42)
+
+    # Preprocess numeric columns
+    numeric_features = ['SEARCH_ENGINE','SOCIAL_MEDIA','VIDEO','EMAIL']
+    numeric_transformer = Pipeline(steps=[('poly',PolynomialFeatures(degree = 2)),('scaler', StandardScaler())])
+    preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, numeric_features)])
+
+    # Create pipeline and train
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor),('classifier', LinearRegression(n_jobs=-1))])
+    model = GridSearchCV(pipeline, param_grid={}, n_jobs=-1, cv=10)
+    model.fit(X_train, y_train)
+
+    # Upload trained model to a stage
+    model_file = os.path.join('/tmp', 'model.joblib')
+    dump(model, model_file)
+    session.file.put(model_file, "@ml_models",overwrite=True)
+
+    # Return model R2 score on train and test data
+    return {"R2 score on Train": model.score(X_train, y_train),"R2 score on Test": model.score(X_test, y_test)}
   EOT
 }
 
